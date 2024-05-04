a79d6de8b22274b39a11faf2611938e9103a67bc
Anh Hào <118757132+anhhao111203@users.noreply.github.com>
2024-05-04 16:47:55 +0700

    Add files via upload

---
+++ MIDTERM/SubForms/UserForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIDTERM
+{
+    public partial class UserForm : Form
+    {
+        public UserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UserForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchTitle_lbl_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void searching_btn_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
